b9260c48e62279987802595c8b260a37b3888e39
bHo321 <37441816+bHo321@users.noreply.github.com>
2019-10-23 23:19:43 -0700

    Update total.c

--- add.c
+++ add.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int a,sum=0,n,i;
+	int num,sum=0,n,i=0;
 	printf("How much no you want to add : ");
 	scanf("%d",&n);
-	for(i=0;i<n;i++)
+	while(i<n)
 	{
 		printf("Enter Number : ");
-		scanf("%d",&a);
-		sum += a;
+		scanf("%d",&num);
+		sum =sum+ num;
 	}
 	printf("sum = %d",sum);
 }
